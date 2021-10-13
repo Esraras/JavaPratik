5d5eb3ea4a5628044b87aff489e24ea8aab3ab4d
esraras <eesraras29@gmail.com>
2021-10-13 19:53:19 +0300

    Added new class

---
+++ src/com/company/VücutKitleİndeksi.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+
+public class VücutKitleİndeksi {
+    public static void main(String[] args) {
+        double kilo, boy ,indeks;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Kilonuzu giriniz : ");
+        kilo = inp.nextDouble();
+        System.out.print("Boyunuzu giriniz : ");
+        boy = inp.nextDouble();
+
+        indeks = kilo / (boy*boy);
+        System.out.println("Vücut kitle indeksiniz : " + indeks);
+
+    }
+}
