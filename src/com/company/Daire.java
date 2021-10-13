4c4cd30cb32543023c0c81d52a2bff07eb3f5d44
esraras <eesraras29@gmail.com>
2021-10-13 17:56:47 +0300

    Added Daire

---
+++ src/com/company/Daire.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+
+public class Daire {
+    public static void main(String[] args) {
+        int r, a;
+        double pi = 3.14, alan,cevre, da;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yarıçap Giriniz : ");
+        r = input.nextInt();
+        System.out.print("Merkez Açısı Giriniz : ");
+        a = input.nextInt();
+
+        alan = pi*r*r;
+        cevre = 2*pi*r;
+
+        //(𝜋 * (r*r) * 𝛼) / 360
+        da = (pi*(r*r)*a) / 360;
+
+        System.out.println("Dairenin alanı : " + alan);
+        System.out.println("Dairenin çevresi : " + cevre);
+        System.out.println("Dilimin alanı : " + da);
+
+
+    }
+}
