7d8c9d8f44bcfa0a783aeb9f769d19161a2e199d
esraras <eesraras29@gmail.com>
2021-10-13 13:41:07 +0300

    finished

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int mat, fizik, kimya, türkce, tarih, müzik;
+
+        System.out.print("Matematik notunuzu giriniz : ");
+        mat = input.nextInt();
+        System.out.print("Fizik notunuzu giriniz : ");
+        fizik = input.nextInt();
+        System.out.print("Kimya notunuzu giriniz : ");
+        kimya = input.nextInt();
+        System.out.print("Türkçe notunuzu giriniz : ");
+        türkce = input.nextInt();
+        System.out.print("Tarih notunuzu giriniz : ");
+        tarih = input.nextInt();
+        System.out.print("Müzik notunuzu giriniz : ");
+        müzik = input.nextInt();
+
+        int result = (müzik+fizik+kimya+türkce+tarih+müzik)/6;
+        String başarı = result > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(başarı);
+
+
+    }
+}
